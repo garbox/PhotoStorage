3b0c492a453275d677d668a8d9fdb75e09db2089
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-22 19:19:35 -0600

    Update home-nav.blade.php

--- resources/views/components/home-nav.blade.php
+++ resources/views/components/home-nav.blade.php
@@ -1,7 +1,7 @@
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light ">
     <div class="container">
-        <a class="navbar-brand" href="{{ Auth::user() ? route('dashboard'): '/' }}">Posto</a>
+        <a class="navbar-brand" href="{{ Auth::user() ? route('dashboard'): '/' }}">Phosto</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
