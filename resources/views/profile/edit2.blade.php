f121c7c957593b7affcc6eb2d8139d3473515c33
Kyle Metivier <metivier.kyle@yahoo.com>
2025-03-17 10:59:19 -0500

    Update edit2.blade.php

--- resources/views/profile/edit2.blade.php
+++ resources/views/profile/edit2.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile - Photo Storage</title>
     <!-- Link to Bootstrap 5 CSS -->
-    <link rel="stylesheet" href="{{ asset('storage/css/bootstrap.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .profile-container {
             margin-top: 50px;
